8e14bccd68338242d6b3b0343b95106fe04942c0
vigu <kvigneshwaranit@gmail.com>
2021-09-14 12:19:11 +0530

    complete the house class

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -19,6 +19,47 @@ class Student {
         }
     }
 }
-const s1 = new Student('1234', 'a', 'v', 1);
-const s2 = new Student('1235', 'b', 'nv', 1);
-console.log(s1.display(), s2.display())
+
+class Houses {
+    maxAllowedStudents = 3;
+    
+    constructor(list) {
+        this.sudentList = list;
+        this.houseList = this.add(list)
+    }
+    add(list) {
+
+        let houseList = {
+            AVeg: [],
+            BVeg: [],
+            ANonVeg: [],
+            BNonVeg: [],
+            NotAvailable: []
+        }
+        list.forEach((student) => {
+            const house = student.house;
+            if (this.isAvailable(houseList[house])) {
+                houseList[house].push(student);
+            } else {
+                student.house = 'NotAvailable';
+                houseList['NotAvailable'].push(student)
+            }
+        });
+        return houseList;
+    }
+
+    isAvailable(arr) {
+        return arr.length < this.maxAllowedStudents ? true : false;
+    }
+
+    display() {
+        console.log(this.houseList);
+    }
+}
+
+const s1 = new Student('1234', 'A', 'Veg', 1);
+const s2 = new Student('1235', 'B', 'NonVeg', 2);
+console.log(s1.display(), s2.display());
+const h = new Houses([s1, s2, s1, s2, s1, s2, s1, s2]);
+// h.add()
+console.log(h.display())
